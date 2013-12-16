9602f3dd102c9559bd67aca1d443617f05118369
Pavel Smirnov <pasmpasm@dhp-12.cs.helsinki.fi>
2013-12-16 13:41:47 +0200

    Ensimmäinen dedis on kunnosa

---
+++ RottaPeli/src/main/java/rottapeli/App.java
@@ -0,0 +1,13 @@
+package rottapeli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
