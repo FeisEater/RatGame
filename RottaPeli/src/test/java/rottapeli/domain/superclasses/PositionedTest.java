9a5a9b7804176e36bb43b95c556a3f3023b87fbc
Pavel Smirnov <pavelsmirnov94@gmail.com>
2013-12-25 17:00:48 +0200

    Asioita voi nyt laittaa oikein, ei väärin v3

---
+++ RottaPeli/src/test/java/rottapeli/domain/superclasses/PositionedTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rottapeli.domain.superclasses;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pavel
+ */
+public class PositionedTest {
+    
+    public PositionedTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+/*    @Test
+    public void hello()
+    {
+    }*/
+}
