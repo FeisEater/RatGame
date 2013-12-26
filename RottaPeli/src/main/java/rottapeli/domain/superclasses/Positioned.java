ace47cf3cab309f83c104810bab0698bbfd2c1c4
Pavel Smirnov <pavelsmirnov94@gmail.com>
2013-12-26 18:06:49 +0200

    testejä entiteetin asettamisesta

--- RottaPeli/src/main/java/rottapeli/domain/superclasses/Positioned.java
+++ RottaPeli/src/main/java/rottapeli/domain/superclasses/Positioned.java
@@ -4,6 +4,7 @@ package rottapeli.domain.superclasses;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 import rottapeli.resource.Const;
@@ -48,20 +49,31 @@ public class Positioned extends Entity {
         List<Entity> entities = getEntities().getList(null);
         Deque<Double> xQueue = new ArrayDeque<Double>();
         Deque<Double> yQueue = new ArrayDeque<Double>();
+        List<Double> usedX = new ArrayList<Double>();
+        List<Double> usedY = new ArrayList<Double>();
         
         xQueue.add(x);
         yQueue.add(y);
+        boolean firstLoop = true;
         while (!xQueue.isEmpty())
         {
+            if (firstLoop)  firstLoop = false;
+            else
+            {
+                usedX.add(x);
+                usedY.add(y);
+            }
             x = xQueue.poll();
             y = yQueue.poll();
+            
             boolean noCollisions = true;
             for (Entity other : entities)
             {
                 if (collidesWith((Positioned)other))
                 {
                     noCollisions = false;
-                    getNearbyPositions(xQueue, yQueue);
+                    getNearbyPositions(xQueue, yQueue, usedX, usedY);
+                    break;
                 }
             }
             if (noCollisions)    return;
@@ -75,31 +87,33 @@ public class Positioned extends Entity {
         return Tools.isInside(this, other) || Tools.isInside(other, this);
     }
 
-    public void getNearbyPositions(Deque<Double> xQueue, Deque<Double> yQueue)
+    public void getNearbyPositions(Deque<Double> xQueue, Deque<Double> yQueue, List<Double> usedX, List<Double> usedY)
     {
-        if (x + getWidth() > Const.width)
-        {
-            xQueue.add(x + 2 * getWidth());
-            yQueue.add(y);
-        }
+        if (x + getWidth() > Const.width ||
+            y + getHeight() > Const.height ||
+            x < 0 || y < 0)
+            return;
 
-        if (y + getHeight() > Const.height)
+        if (usedX != null && usedY != null)
         {
-            xQueue.add(x);
-            yQueue.add(y + 2 * getHeight());
+            for (int i = 0; i < usedX.size(); i++)
+            {
+                if (usedX.get(i) == x && usedY.get(i) == y)
+                    return;
+            }
         }
+        
+        xQueue.add(x + 2 * getWidth());
+        yQueue.add(y);
 
-        if (x < 0)
-        {
-            xQueue.add(x - 2 * getWidth());
-            yQueue.add(y);
-        }
+        xQueue.add(x);
+        yQueue.add(y + 2 * getHeight());
 
-        if (y < 0)
-        {
-            xQueue.add(x);
-            yQueue.add(y - 2 * getHeight());
-        }
+        xQueue.add(x - 2 * getWidth());
+        yQueue.add(y);
+
+        xQueue.add(x);
+        yQueue.add(y - 2 * getHeight());
     }
     
     public void draw(Graphics g, double xMultiplier, double yMultiplier)
--- RottaPeli/src/test/java/rottapeli/domain/superclasses/PositionedTest.java
+++ RottaPeli/src/test/java/rottapeli/domain/superclasses/PositionedTest.java
@@ -4,12 +4,19 @@
  */
 package rottapeli.domain.superclasses;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import rottapeli.peli.EntityList;
+import rottapeli.resource.Const;
+import rottapeli.resource.Tools;
 
 /**
  *
@@ -36,8 +43,155 @@ public class PositionedTest {
     public void tearDown() {
     }
 
-/*    @Test
-    public void hello()
+    @Test
+    public void getsNearbyPositions1()
     {
-    }*/
+        Positioned p = new Positioned(64,64,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        p.getNearbyPositions(xQueue, yQueue, null, null);
+        assertTrue(xQueue.toString().equals("[66.0, 64.0, 62.0, 64.0]") &&
+                yQueue.toString().equals("[64.0, 66.0, 64.0, 62.0]"));
+    }
+    @Test
+    public void getsNearbyPositions2()
+    {
+        Positioned p = new Positioned(0,0,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        p.getNearbyPositions(xQueue, yQueue, null, null);
+        assertTrue(xQueue.toString().equals("[2.0, 0.0, -2.0, 0.0]") &&
+                yQueue.toString().equals("[0.0, 2.0, 0.0, -2.0]"));
+    }
+    @Test
+    public void getsNearbyPositions3()
+    {
+        Positioned p = new Positioned(255,191,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        p.getNearbyPositions(xQueue, yQueue, null, null);
+        assertTrue(xQueue.toString().equals("[257.0, 255.0, 253.0, 255.0]") &&
+                yQueue.toString().equals("[191.0, 193.0, 191.0, 189.0]"));
+    }
+    @Test
+    public void dontGetNearbyPositionsIfOutOfBounds1()
+    {
+        Positioned p = new Positioned(-1,64,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        p.getNearbyPositions(xQueue, yQueue, null, null);
+        assertTrue(xQueue.toString().equals("[]") &&
+                yQueue.toString().equals("[]"));
+    }
+    @Test
+    public void dontGetNearbyPositionsIfOutOfBounds2()
+    {
+        Positioned p = new Positioned(64,-1,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        p.getNearbyPositions(xQueue, yQueue, null, null);
+        assertTrue(xQueue.toString().equals("[]") &&
+                yQueue.toString().equals("[]"));
+    }
+    @Test
+    public void dontGetNearbyPositionsIfOutOfBounds3()
+    {
+        Positioned p = new Positioned(Const.width,64,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        p.getNearbyPositions(xQueue, yQueue, null, null);
+        assertTrue(xQueue.toString().equals("[]") &&
+                yQueue.toString().equals("[]"));
+    }
+    @Test
+    public void dontGetNearbyPositionsIfOutOfBounds4()
+    {
+        Positioned p = new Positioned(64,Const.height,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        p.getNearbyPositions(xQueue, yQueue, null, null);
+        assertTrue(xQueue.toString().equals("[]") &&
+                yQueue.toString().equals("[]"));
+    }
+    @Test
+    public void dontGetNearbyPositionIfAlreadyWasThere()
+    {
+        Positioned p = new Positioned(64,64,1,1);
+        EntityList list = new EntityList();
+        list.addEntity(p);
+        Deque<Double> xQueue = new ArrayDeque<Double>();
+        Deque<Double> yQueue = new ArrayDeque<Double>();
+        List<Double> usedX = new ArrayList<Double>();
+        List<Double> usedY = new ArrayList<Double>();
+        usedX.add(1.0);
+        usedX.add(2.0);
+        usedX.add(64.0);
+        usedX.add(3.0);
+        usedY.add(71.0);
+        usedY.add(37.0);
+        usedY.add(64.0);
+        usedY.add(43.0);
+        p.getNearbyPositions(xQueue, yQueue, usedX, usedY);
+        assertTrue(xQueue.toString().equals("[]") &&
+                yQueue.toString().equals("[]"));
+    }
+
+    @Test
+    public void ifFreeSpotJustPlaceIt()
+    {
+        Positioned p = new Positioned(64,64,1,1);
+        p.findNearestFreeSpot();
+        assertTrue(p.X() == 64 && p.Y() == 64);
+    }
+    @Test
+    public void ifSpotNotFreePlaceNearBy()
+    {
+        EntityList list = new EntityList();
+        Positioned p = new Positioned(64,64,1,1);
+        list.addEntity(p);
+        list.addEntity(new Positioned(64,64,1,1));
+        p.findNearestFreeSpot();
+        assertTrue( (p.X() == 66 && p.Y() == 64) ||
+                    (p.X() == 64 && p.Y() == 66) ||
+                    (p.X() == 62 && p.Y() == 64) ||
+                    (p.X() == 64 && p.Y() == 62));
+    }
+    @Test
+    public void findWhichNearbySpotIsFree()
+    {
+        EntityList list = new EntityList();
+        Positioned p = new Positioned(64,64,1,1);
+        list.addEntity(p);
+        list.addEntity(new Positioned(48,64,128,128));
+        p.findNearestFreeSpot();
+        assertTrue(p.X() == 64 && p.Y() == 62);
+    }
+    @Test
+    public void findWhichNearbySpotIsFree2()
+    {
+        EntityList list = new EntityList();
+        Positioned p = new Positioned(64,64,1,1);
+        list.addEntity(p);
+        list.addEntity(new Positioned(48,48,32,32));
+        p.findNearestFreeSpot();
+        assertTrue( (p.X() == 82 && p.Y() == 64) ||
+                    (p.X() == 64 && p.Y() == 82) ||
+                    (p.X() == 46 && p.Y() == 64) ||
+                    (p.X() == 64 && p.Y() == 46));
+    }
+
 }
